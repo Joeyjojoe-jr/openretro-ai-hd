a9f9ffc5d9e44390eccf9ae1418279d09daacf78
wweiler <wweiler@alumni.nd.edu>
2025-05-20 16:02:21 +0000

    Deleted duplicative file folder named unzippedopenretro_ai_hd_complete_github_bundle.

--- unzippedopenretro_ai_hd_complete_github_bundle/agents/agent_registry.py
+++
@@ -1,7 +0,0 @@
-AGENT_REGISTRY = {}
-
-def register_agent(name):
-    def wrapper(cls):
-        AGENT_REGISTRY[name] = cls
-        return cls
-    return wrapper
--- unzippedopenretro_ai_hd_complete_github_bundle/agents/base_agent.py
+++
@@ -1,3 +0,0 @@
-class BaseAgent:
-    def run(self, *args, **kwargs):
-        raise NotImplementedError("Each agent must implement a run method.")
